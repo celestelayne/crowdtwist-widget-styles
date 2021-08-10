9a3e6a88b00b84e2ba16a47eb8c1a68e9802e663
Celeste Layne <layne.celeste@gmail.com>
2021-08-10 16:34:32 -0400

    add reload widget functionality

--- widget-script.js
+++ widget-script.js
@@ -1,1 +1,13 @@
 console.log("this works");
+
+const reloadWidgets = () => {
+  if (window.CrowdTwist) return;
+
+  let widgetCount = window.CrowdTwist.Widget._widgets.length;
+
+  for (let i = widgetCount; i > 0; i--) {
+    window.CrowdTwist.Widget.removeWidget(CrowdTwist.Widget._widgets[0]);
+  }
+
+  window.CrowdTwist.loadWidgets();
+};
